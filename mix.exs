7c2276329cd908aa392f90ce845cb019cbe217ea
Timmo Verlaan <timmo@timmo.immo>
2023-05-05 17:47:45 +0200

    earmark 1.4 dependency requires at least elixir 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RadiusProxy.Mixfile do
     [
       app: :elixir_radius,
       version: "1.0.0",
-      elixir: "~> 1.0",
+      elixir: "~> 1.12",
       description: desc(),
       package: package(),
       deps: deps()
