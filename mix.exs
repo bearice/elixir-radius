40af834fabacdb63eb31ba0095787acf85e18d92
Bearice Ren <bearice@icybear.net>
2023-06-07 20:03:52 +0900

    Bump v2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RadiusProxy.Mixfile do
   def project do
     [
       app: :elixir_radius,
-      version: "1.2.0",
+      version: "2.0.0",
       elixir: "~> 1.12",
       description: desc(),
       package: package(),
